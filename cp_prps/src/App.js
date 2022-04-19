19a1a5b5cca2f1bf630a2426a93df03515b0b8d6
BENHALIMAmohamed <benhalimamohamed.ca@gmail.com>
2022-04-19 09:36:02 +0100

    better idea for handle_name

--- cp_prps/src/App.js
+++ cp_prps/src/App.js
@@ -9,9 +9,8 @@ function App() {
     bio: "16/11/1992",
     profession: "petroleum engineer",
   };
-  const handle_name = () => {
-    alert(`this is the full name ${profile_data.fullName}`);
-  };
+  const handle_name = (x) => alert(`this is the full name ${x}`);
+  
   const style_css = {color:"red"};
   return (
     <div className="App">
